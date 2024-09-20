d2e0dfa27530c3ca8825c01152f4748718b502ec
Wish <100239673+wish13yt@users.noreply.github.com>
2024-09-20 18:00:39 -0500

    add textbox 	modified:   main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from guizero import App, Text, PushButton
+from guizero import App, Text, PushButton, TextBox
+def say_hello():
+  message.value = "Hello"
 def change_message():
     message.value = "Hello Button!"
 app = App(title="GUITest")
 text = Text(app, text="GUI World!")
 button = PushButton(app, text="Press me!", command=change_message)
 message = Text(app, text="Please press the button above!")
+name = TextBox(app)
+button = PushButton(app, text="Submit input!", command=say_hello)
+change = Text(app, text="Input 'Hello World' in the text box above.")
 app.display()
