c095f201563721db345a67c8574b158084ae4a2f
Wish <100239673+wish13yt@users.noreply.github.com>
2024-09-20 17:54:49 -0500

    add button, text modified:   main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from guizero import App, Text
-
+from guizero import App, Text, PushButton
+def change_message():
+    message.value = "Hello Button!"
 app = App(title="GUITest")
 text = Text(app, text="GUI World!")
+button = PushButton(app, text="Press me!", command=change_message)
+message = Text(app, text="Please press the button above!")
 app.display()
